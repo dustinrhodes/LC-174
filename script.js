556096e5027c9fd0a0e90a06414e85ca4e44c4f1
Dustin Rhodes <dustin.rhodes@lacity.org>
2015-11-09 11:47:33 -0800

    LC-174 style changes

--- script.js
+++ script.js
@@ -5,22 +5,25 @@ $(document).ready(function() {
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html =  '<div class="day first"><p>'+weather.city+', '+weather.region+' - '+weather.updated+'</p>';
+      html =  '<div class="today"><p>'+weather.city+', '+weather.region+' - '+weather.updated+'</p>';
       html += '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<h3>'+weather.text+'</h3>';
       html += '<ul><li class="high"> High: '+weather.high+'&deg;'+weather.units.temp+'</li>';
-      html += '<li class="low"> Low: '+weather.low+'&deg;'+weather.units.temp+'</li></ul>';
-      html += '<p>Wind Chill: '+weather.wind.chill+'&deg;'+weather.units.temp+' - Heat Index: '+weather.heatindex+'&deg;'+weather.units.temp+'</p>';
-      html += '<p>Wind: '+weather.wind.speed+ ' mph ' +weather.wind.direction+'</p>';
-      html += '<p>Humidity: '+weather.humidity+'% Barometric Pressure: '+weather.pressure+' in </p>';
+      html += '<li class="low"> Low: '+weather.low+'&deg;'+weather.units.temp+'</li></ul></div>';
 
-      html +=  '<div class="day"><p>'+weather.forecast[1].day+', '+weather.forecast[1].date+'</p>';
+      html += '<div class="data"><p>Wind Chill: '+weather.wind.chill+'&deg;'+weather.units.temp+'</p>';
+      html += '<p>Heat Index: '+weather.heatindex+'&deg;'+weather.units.temp+'</p>';
+      html += '<p>Wind: '+weather.wind.speed+ ' mph ' +weather.wind.direction+'</p>';
+      html += '<p>Humidity: '+weather.humidity+'%</p>';
+      html += '<p>Barometric Pressure: '+weather.pressure+' in</p></div>';
+
+      html += '<div class="forecast"><p>'+weather.forecast[1].day+', '+weather.forecast[1].date+'</p>';
       html += '<h2><i class="icon-'+weather.forecast[1].code+'"></i></h2>';
       html += '<h3>'+weather.forecast[1].text+'</h3>';
       html += '<ul><li class="high"> High: '+weather.forecast[1].high+'&deg;'+weather.units.temp+'</li>';
       html += '<li class="low"> Low: '+weather.forecast[1].low+'&deg;'+weather.units.temp+'</li></ul></div>';
 
-      html +=  '<div class="day last"><p>'+weather.forecast[2].day+', '+weather.forecast[2].date+'</p>';
+      html += '<div class="forecast"><p>'+weather.forecast[2].day+', '+weather.forecast[2].date+'</p>';
       html += '<h2><i class="icon-'+weather.forecast[2].code+'"></i></h2>';
       html += '<h3>'+weather.forecast[2].text+'</h3>';
       html += '<ul><li class="high"> High: '+weather.forecast[2].high+'&deg;'+weather.units.temp+'</li>';
